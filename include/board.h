3554fd97df6d0b3fca28ffab8b4c340239647a0f
subhrajyoti <subhra209@gmail.com>
2015-05-17 10:05:12 +0530

    project creation

---
+++ include/board.h
@@ -0,0 +1,217 @@
+ #ifndef DEVICE_H
+#define DEVICE_H
+
+/*
+*------------------------------------------------------------------------------
+* device.h
+*
+* Include file for port pin assignments
+*
+
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* File				: device.h
+*------------------------------------------------------------------------------
+*
+
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Include Files
+*------------------------------------------------------------------------------
+*/
+
+#include <p18f4520.h>
+#include <delays.h>
+#include <timers.h>
+#include "typedefs.h"
+#include "config.h"
+
+
+
+
+/*
+*------------------------------------------------------------------------------
+* Hardware Port Allocation
+*------------------------------------------------------------------------------
+*/
+
+//  UART  Association
+
+#define 	SER1_TX					PORTCbits.RC6 		// serial transmit
+#define		SER1_TX_DIR				TRISCbits.TRISC6
+#define 	SER1_RX					PORTCbits.RC7			// serial receive
+#define		SER1_RX_DIR				TRISCbits.TRISC7
+
+
+// Heart Beat 
+#define	HEART_BEAT				LATEbits.LATE0
+#define	HEART_BEAT_DIR			TRISEbits.TRISE0				
+	
+
+//Tower Lamp Configaration
+
+#define LAMP_GREEN				LATAbits.LATA1
+#define LAMP_GREEN_DIR			TRISAbits.TRISA1
+
+#define LAMP_YELLOW				LATAbits.LATA2
+#define LAMP_YELLOW_DIR			TRISAbits.TRISA2
+
+
+#define LAMP_RED				LATAbits.LATA3
+#define LAMP_RED_DIR			TRISAbits.TRISA3
+
+
+#define	LAMP_BLUE				LATAbits.LATA4
+#define	LAMP_BLUE_DIR			TRISAbits.TRISA4
+
+	
+/*
+*------------------------------------------------------------------------------
+* Public Defines
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Public Macros
+*------------------------------------------------------------------------------
+*/
+
+#define DEVICE_ID			(0x01)
+
+
+#define SYSTEM_CLOCK			(32000000UL)	// Hz		// 8MHz internal oscillator
+
+//#define SYSTEM_CLOCK			(40000000UL)	// Hz		// 10MHz external crystal x4 PLL
+
+#define PERIPHERA_CLOCK			(SYSTEM_CLOCK / 4)UL
+
+#define PERIPHERA_CLOCK_PERIOD 	(1 / PERIPHERA_CLOCK)UL
+
+
+
+// Direction controle bit is processor specific ,
+#define PORT_OUT				(BOOL)(0)
+#define PORT_IN					(0XFF)
+
+#define OFF_FOREVER				(BOOL)(0)
+#define LOOP_FOREVER			(BOOL)(1)
+
+#define SWITCH_OFF				(BOOL)(0)
+#define SWITCH_ON				(BOOL)(1)
+
+
+#define DISPLAY_DISABLE			(BOOL)(1)
+#define DISPLAY_ENABLE			(BOOL)(0)
+void EnableInterrupts(void);
+
+/*
+#ifdef __74LS374__
+#undef DISPLAY_DISABLE
+#undef DISPLAY_ENABLE
+
+#define DISPLAY_DISABLE			(BOOL)(0)
+#define DISPLAY_ENABLE			(BOOL)(1)
+
+#endif 			//__74LS374__
+*/
+
+
+#define GetSystemClock()		(SYSTEM_CLOCK)      // Hz
+#define GetInstructionClock()	(GetSystemClock()/4)
+#define GetPeripheralClock()	GetInstructionClock()
+
+
+#define BUZZER_ON()			BUZ_OP = SWITCH_ON;
+#define BUZZER_OFF()		BUZ_OP = SWITCH_OFF;
+
+// Disable global interrupt bit.
+#define ENTER_CRITICAL_SECTION()	INTCONbits.GIE = 0;
+
+// Enable global interrupt bit.
+#define EXIT_CRITICAL_SECTION()		INTCONbits.GIE = 1;
+
+#define ENABLE_GLOBAL_INT()			EXIT_CRITICAL_SECTION()
+
+
+#define DISABLE_ADC_INTERRUPT()		PIE1bits.ADIE = 0
+#define ENABLE_ADC_INTERRUPT()		PIE1bits.ADIE = 1
+
+#define DISABLE_INT0_INTERRUPT()	INTCONbits.INT0IE = 0
+#define ENABLE_INT0_INTERRUPT()		INTCONbits.INT0IE = 1
+#define CLEAR_INTO_INTERRUPT()		INTCONbits.INT0IF = 0
+
+
+#define DISABLE_TMR0_INTERRUPT()	INTCONbits.TMR0IE = 0
+#define ENABLE_TMR0_INTERRUPT()		INTCONbits.TMR0IE = 1
+
+#define DISABLE_TMR1_INTERRUPT()	PIE1bits.TMR1IE = 0
+#define ENABLE_TMR1_INTERRUPT()		PIE1bits.TMR1IE = 1
+
+
+#define DISABLE_UART_TX_INTERRUPT()	PIE1bits.TXIE = 0
+#define ENABLE_UART_TX_INTERRUPT()	PIE1bits.TXIE = 1
+
+#define DISABLE_UART_RX_INTERRUPT()	PIE1bits.RCIE = 0
+#define ENABLE_UART_RX_INTERRUPT()	PIE1bits.RCIE = 1
+
+
+#define HOLD_ATKBD_CLOCK()		    ATKBD_CLK_DIR= PORT_OUT;\
+									ATKBD_CLK = 0;
+
+#define RELEASE_ATKBD_CLOCK()	  	ATKBD_CLK_DIR = PORT_IN;\
+									ATKBD_CLK = 1;
+
+#define SET_INT0_FALLING_EDGE_TRIGGER()	INTCON2bits.INTEDG0 = 0
+
+#define	ATKBD_INTERRUPT_EDGE	INTCON2.INTEDG0		
+
+#define ENB_485_TX()	TX_EN = 1;
+#define ENB_485_RX()	TX_EN = 0;
+
+#define Delay10us(us)		Delay10TCYx(((GetInstructionClock()/1000000)*(us)))
+#define DelayMs(ms)												\
+	do																\
+	{																\
+		unsigned int _iTemp = (ms); 								\
+		while(_iTemp--)												\
+			Delay1KTCYx((GetInstructionClock()+999999)/1000000);	\
+	} while(0)
+
+/*
+*------------------------------------------------------------------------------
+* Public Data Types
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Public Variables (extern)
+*------------------------------------------------------------------------------
+*/
+
+
+/*
+*------------------------------------------------------------------------------
+* Public Constants (extern)
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Public Function Prototypes (extern)
+*------------------------------------------------------------------------------
+*/
+
+extern void BRD_init(void);
+
+#endif
+/*
+*  End of device.h
+*/
---
+++ include/communication.h
@@ -0,0 +1,52 @@
+
+#ifndef __COMMUNICATION__
+#define __COMMUNICATION__
+
+#include "board.h"
+
+
+typedef enum
+{
+	COM_RESP_NONE = 0xFF,
+	COM_RESP_OK = 0,
+	COM_RESP_OVERRUN = 0x90,
+	COM_RESP_CHECKSUM_ERROR = 0x91,
+	COM_RESP_INVALID_CMD = 0x92, 
+	COM_RESP_INVALID_DATA = 0x93
+}COM_RESP_CODE;
+
+
+
+
+
+
+enum 
+{ 
+	COM_RESET = 0,
+	COM_START = 1,
+	COM_IN_PACKET_COLLECTION = 2,
+	COM_IN_TX_DATA = 3
+};
+
+
+
+
+enum 
+{
+	COM_DEVICE_ADDRESS_INDEX = 0,
+	COM_TX_CODE_INDEX = 1,
+	COM_TX_DATA_START_INDEX = 2,
+	COM_RX_DATA_START_INDEX = 2
+};
+
+
+
+
+
+void COM_init(UINT8 cmd_sop , UINT8 cmd_eop ,UINT8 resp_sop , UINT8 resp_eop , UINT8 (*callBack)(UINT8* rxdata, UINT8* txCode,UINT8** txPacket));
+UINT8 APP_comCallBack( far UINT8 *rxPacket, far UINT8* txCode,far UINT8** txPacket);
+void COM_task(void);
+void COM_txStr(rom UINT8 *str);
+#endif
+
+
